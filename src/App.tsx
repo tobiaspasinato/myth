2838c2fb17d25b5b067ff49706b9b2b273765132
tobiaspasinato <tobiasmarpasinato@gmail.com>
2025-08-20 06:15:01 -0300

    2

--- src/App.tsx
+++ src/App.tsx
@@ -11,6 +11,7 @@ function App() {
       <Routes>
         <Route path="/" element={<Home />} />
         <Route path="/about" element={<div>About</div>} />
+        <Route path="/contact" element={<div>Contact</div>} />
       </Routes>
     </Layout>
   )
--- src/components/ui/NavBar.tsx
+++ src/components/ui/NavBar.tsx
@@ -2,12 +2,21 @@ import React, { useState } from 'react';
 import { Link } from 'react-router-dom';
 import './NavBar.css';
 import searchIcon from '../../assets/search-icon.svg';
-// import darkModeIcon from '../../assets/dark-mode.svg';
-// import lightModeIcon from '../../assets/light-mode.svg';
+import darkModeIcon from '../../assets/dark-mode.svg';
+import lightModeIcon from '../../assets/light-mode.svg';
 
 export default function NavBar() {
 
     const [searchTerm, setSearchTerm] = useState('');
+    const [colorMode, setColorMode] = useState('light');
+
+    function handleSearch(searchTerm : string) {
+        alert(searchTerm);
+    }
+
+    function toggleColorMode() {
+        setColorMode((prevMode) => (prevMode === 'light' ? 'dark' : 'light'));
+    }
 
     return (
         <div className='navbar'>
@@ -20,11 +29,13 @@ export default function NavBar() {
             </ul>
             <div className='search-box'>
                 <input type="text" placeholder="Search" value={searchTerm} onChange={(e) => setSearchTerm(e.target.value)}/>
-                <button type="button" className='search-button' onClick={() => {}}>
+                <button type="button" className='search-button' onClick={() => handleSearch(searchTerm)}>
                     <img src={searchIcon} alt="Search Icon" className='search-icon'/>
                 </button>
             </div>
-            {/* <img src={lightModeIcon} alt="Light Mode Icon" className='toggle-icon'/> */}
+            <button type="button" className='toggle-button' onClick={toggleColorMode}>
+                <img src={colorMode === 'light' ? darkModeIcon : lightModeIcon} alt="Toggle Color Mode Icon" className='toggle-icon'/>
+            </button>
         </div>
     )
 }
